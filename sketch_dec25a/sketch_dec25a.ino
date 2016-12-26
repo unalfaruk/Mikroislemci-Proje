a79db2ff1f58d3ec46f5f9a798183876ccfbbc0b
unalfaruk <unalfaruk@outlook.com>
2016-12-27 02:50:25 +0300

    Motor kontrolu eklendi.

--- sketch_dec25a/sketch_dec25a.ino
+++ sketch_dec25a/sketch_dec25a.ino
@@ -1,8 +1,10 @@
 int trigPin=7;
 int echoPin=6;
-int motorPin=3;
+int motor=2;
 int yesilLed=12;
 int kirmiziLed=13;
+int ses=11;
+
 int oncekiDurum;
 int simdikiDurum;
 int sayac=0;
@@ -13,25 +15,22 @@ void uyariSistemi(int uyariSeviyesi, int mesafe=0) {
   switch(uyariSeviyesi){
     
     case 0:
-      //simdikiDurum = 0; //En tehlikeli durum için 0 kodu
       Serial.println("100 altı");      
       digitalWrite(kirmiziLed, HIGH);
+      digitalWrite(motor, HIGH);
       break;
     
     case 1:
-      //simdikiDurum = 1;
       Serial.println("100 150 arası");
       digitalWrite(kirmiziLed, HIGH);
       break;
     
     case 2:
-      //simdikiDurum = 2;
       Serial.println("150 200 arası");
       digitalWrite(kirmiziLed, HIGH);
       break;
     
     case 3:
-      //simdikiDurum = 3; //Tehlikeli olmayan durum için 3 kodu
       Serial.println("Temiz");      
       digitalWrite(yesilLed, HIGH);
       break;
@@ -42,9 +41,15 @@ void uyariSistemi(int uyariSeviyesi, int mesafe=0) {
 
 }
 
+/*
+Ana döngüden 2 döngüde bir gelen simdiki ve onceki durum değerlerini alıyoruz.
+İki durumu karşılaştırıp sonuca göre uyarı sistemini çalıştıracağız.
+ */
+
 void kontrolEt(int simdiki,int onceki=3){
   digitalWrite(yesilLed, LOW);
   digitalWrite(kirmiziLed, LOW);
+  digitalWrite(motor, LOW);
   
   if(onceki==simdiki){
     uyariSistemi(simdiki);
@@ -75,34 +80,36 @@ void loop() {
   digitalWrite(trigPin, LOW);
   sure = pulseIn(echoPin, HIGH);
   mesafe = (sure/2) / 29.1;
- 
+
+/*
+Mesafeye göre durumun aciliyetini belirliyoruz.
+*/
     
   if(mesafe>200 || mesafe <0){
     simdikiDurum=3;
-    //uyariSistemi(3);
   }else if(mesafe<200 && mesafe >150){
     simdikiDurum=2;
-    //uyariSistemi(2);
   }else if(mesafe<150 && mesafe >100){
     simdikiDurum=1;
-    //uyariSistemi(1);  
   }else if(mesafe<100){
     simdikiDurum=0;
-    //uyariSistemi(0);
   }  
 
-  /*
-  Her iki dönütte bir kontrol ediyoruz. Sensörümüzden gelebilecek olan hatalı veriyi kontrol
-  etmek için yapıyoruz bunu. Gelen değer bir önceki değer ile aynı değilse stabile olana kadar
-  bekleriz, ve bu süreçte de ikisi arasında tehlikeli olana göre işlem yaparız. Stabileyse doğrudan
-  uyarı sistemi ne yapması gerekiyorsa onu yapmalı.
-  */
+/*
+Her iki dönütte bir kontrol ediyoruz. Sensörümüzden gelebilecek olan hatalı veriyi kontrol
+etmek için yapıyoruz bunu. Gelen değer bir önceki değer ile aynı değilse stabile olana kadar
+bekleriz, ve bu süreçte de ikisi arasında tehlikeli olana göre işlem yaparız. Stabileyse doğrudan
+uyarı sistemi ne yapması gerekiyorsa onu yapmalı.
+*/
   
   sayac++;  
   if(sayac%2==0){
     kontrolEt(simdikiDurum,oncekiDurum);
   }
-  
+
+/*
+Şimdiki durum bir sonraki döngüde eski(önceki) durum olacak.
+ */
   delay(200);
   oncekiDurum = simdikiDurum;
 
