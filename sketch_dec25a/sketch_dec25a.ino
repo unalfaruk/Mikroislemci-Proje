d3faabb4ca4d0db37b30e0673dd371a42fcbb8b4
unalfaruk <unalfaruk@outlook.com>
2016-12-27 00:16:59 +0300

    Ultrasonik sensör algoritması.

---
+++ sketch_dec25a/sketch_dec25a.ino
@@ -0,0 +1,38 @@
+int trigPin=7;
+int echoPin=6;
+int motorPin=3;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+}
+
+void loop() {
+  //digitalWrite(motorPin,HIGH);
+  // put your main code here, to run repeatedly:
+  long sure, mesafe;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  sure = pulseIn(echoPin, HIGH);
+  mesafe = (sure/2) / 29.1;
+     if (mesafe > 200 || mesafe < 0){
+     digitalWrite(motorPin,HIGH);
+     Serial.println("Menzil Disi");
+     }
+     else if(mesafe<20) {
+     digitalWrite(motorPin,LOW);
+     Serial.print(mesafe);
+     Serial.println(" cm");
+     }else{
+     digitalWrite(motorPin,HIGH);
+     Serial.println(mesafe);
+     }
+  delay(500);
+
+}
