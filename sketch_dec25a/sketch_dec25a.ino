4e957dff84fc6397ac793c0791bdfacc5ce26e9f
unalfaruk <unalfaruk@outlook.com>
2016-12-27 02:18:51 +0300

    Daha dogru sonuc almak ıcın algorıtma duzeltıldı, aciklama satirlari eklendi.

--- sketch_dec25a/sketch_dec25a.ino
+++ sketch_dec25a/sketch_dec25a.ino
@@ -1,6 +1,61 @@
 int trigPin=7;
 int echoPin=6;
 int motorPin=3;
+int yesilLed=12;
+int kirmiziLed=13;
+int oncekiDurum;
+int simdikiDurum;
+int sayac=0;
+
+
+void uyariSistemi(int uyariSeviyesi, int mesafe=0) {
+
+  switch(uyariSeviyesi){
+    
+    case 0:
+      //simdikiDurum = 0; //En tehlikeli durum için 0 kodu
+      Serial.println("100 altı");      
+      digitalWrite(kirmiziLed, HIGH);
+      break;
+    
+    case 1:
+      //simdikiDurum = 1;
+      Serial.println("100 150 arası");
+      digitalWrite(kirmiziLed, HIGH);
+      break;
+    
+    case 2:
+      //simdikiDurum = 2;
+      Serial.println("150 200 arası");
+      digitalWrite(kirmiziLed, HIGH);
+      break;
+    
+    case 3:
+      //simdikiDurum = 3; //Tehlikeli olmayan durum için 3 kodu
+      Serial.println("Temiz");      
+      digitalWrite(yesilLed, HIGH);
+      break;
+    
+    default:
+      Serial.println("Default");
+  }
+
+}
+
+void kontrolEt(int simdiki,int onceki=3){
+  digitalWrite(yesilLed, LOW);
+  digitalWrite(kirmiziLed, LOW);
+  
+  if(onceki==simdiki){
+    uyariSistemi(simdiki);
+  }else{
+    if(simdiki<onceki){
+      uyariSistemi(simdiki);
+    }else{
+      uyariSistemi(onceki);
+    }
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,8 +66,7 @@ void setup() {
 }
 
 void loop() {
-  //digitalWrite(motorPin,HIGH);
-  // put your main code here, to run repeatedly:
+
   long sure, mesafe;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -21,29 +75,35 @@ void loop() {
   digitalWrite(trigPin, LOW);
   sure = pulseIn(echoPin, HIGH);
   mesafe = (sure/2) / 29.1;
-
-  if(mesafe>200 || mesafe <0){
-    Serial.println("Temiz");
-  }else if(mesafe<200 && mesafe >150){
-    Serial.println("150 200 arası");
-  }else if(mesafe<150 && mesafe >100){
-    Serial.println("100 150 arası");
-  }else if(mesafe<100){
-    Serial.println("100 altı");
-  }
+ 
     
-     /*if (mesafe > 200 || mesafe < 0){
-     digitalWrite(motorPin,HIGH);
-     Serial.println("Menzil Disi");
-     }
-     else if(mesafe<20) {
-     digitalWrite(motorPin,LOW);
-     Serial.print(mesafe);
-     Serial.println(" cm");
-     }else{
-     digitalWrite(motorPin,HIGH);
-     Serial.println(mesafe);
-     }*/
+  if(mesafe>200 || mesafe <0){
+    simdikiDurum=3;
+    //uyariSistemi(3);
+  }else if(mesafe<200 && mesafe >150){
+    simdikiDurum=2;
+    //uyariSistemi(2);
+  }else if(mesafe<150 && mesafe >100){
+    simdikiDurum=1;
+    //uyariSistemi(1);  
+  }else if(mesafe<100){
+    simdikiDurum=0;
+    //uyariSistemi(0);
+  }  
+
+  /*
+  Her iki dönütte bir kontrol ediyoruz. Sensörümüzden gelebilecek olan hatalı veriyi kontrol
+  etmek için yapıyoruz bunu. Gelen değer bir önceki değer ile aynı değilse stabile olana kadar
+  bekleriz, ve bu süreçte de ikisi arasında tehlikeli olana göre işlem yaparız. Stabileyse doğrudan
+  uyarı sistemi ne yapması gerekiyorsa onu yapmalı.
+  */
+  
+  sayac++;  
+  if(sayac%2==0){
+    kontrolEt(simdikiDurum,oncekiDurum);
+  }
+  
   delay(200);
+  oncekiDurum = simdikiDurum;
 
 }
